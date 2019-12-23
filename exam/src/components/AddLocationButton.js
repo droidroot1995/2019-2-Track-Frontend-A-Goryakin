51b783873d70af148aae40ec48cb877d0d07de5f
Александр <droidroot@localhost.localdomain>
2019-12-23 21:06:30 +0300

    fixes

--- exam/src/components/AddLocationButton.js
+++ exam/src/components/AddLocationButton.js
@@ -10,8 +10,8 @@ const AddLocationButton = (props) => {
   const { addLoc } = props
   return (
     <div className={styles.add_loc} onClick={() => addLoc()}>
-      <svg xmlns="http://www.w3.org/2000/svg" className={styles.add_loc_svg} viewBox="0 0 512 512">
-        <path d="M290.74 93.24l128.02 128.02-277.99 277.99-114.14 12.6C11.35 513.54-1.56 500.62.14 485.34l12.7-114.22 277.9-277.88zm207.2-19.06l-60.11-60.11c-18.75-18.75-49.16-18.75-67.91 0l-56.55 56.55 128.02 128.02 56.55-56.55c18.75-18.76 18.75-49.16 0-67.91z" />
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className={styles.add_loc_svg}>
+        <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
       </svg>
     </div>
   )
